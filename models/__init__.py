36b8cd488716f55651610b02077c8c10d8cea04a
Salym Senyonga <salymsash@gmail.com>
2017-09-30 15:28:26 +0300

    Export Instruction module
    Add Instruction to list of constants exposed by the models module.

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .base_model import BaseModel
 from .user import User
 from .recipe import Recipe
+from .instruction import Instruction
 
 __all__ = [
     'BaseModel',
     'User',
-    'Recipe'
+    'Recipe',
+    'Instruction',
 ]
