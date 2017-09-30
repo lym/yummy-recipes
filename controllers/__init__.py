28ee71b2eface9b9ea508326172e960b90e84f97
Salym Senyonga <salymsash@gmail.com>
2017-09-30 15:30:09 +0300

    Export InstructionsController constant
    Add InstructionsController to list of constants exposed by the
    controllers module

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,11 @@
 from .users import UsersController
 from .login_controller import LoginController
 from .recipes_controller import RecipesController
+from .instructions_controller import InstructionsController
 
 __all__ = [
     'UsersController',
     'LoginController',
     'RecipesController',
+    'InstructionsController',
 ]
