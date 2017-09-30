9b95af9e1d13031921487043b3c538077538f4c0
Salym Senyonga <salymsash@gmail.com>
2017-09-30 15:26:20 +0300

    Correct instruction unit tests
    Unit tests for the instruction resource were previously in a
    non-functional state. This commit corrects that.

--- test/test_instruction.py
+++ test/test_instruction.py
@@ -1,61 +1,43 @@
 from tinydb import where
 
-from app.models import (
+from models import (
     Recipe,
     Instruction,
     User
 )
 
-recipe = Recipe(883248, 'New Recipe')
-user = User('email@anonmail.com', 'weakpass')
-instruction = Instruction('New Instruction', 118266162315588491253214850569623365886)
+recipe_id   = 234234234234
+user        = User('email@anonmail.com', 'weakpass')
+instruction = Instruction(
+    recipe_id,
+    'New Instruction',
+    118266162315588491253214850569623365886
+)
 
 
 def test_default_attributes():
-    assert recipe.table_name is not None
-
-    assert instruction.id is not None
+    assert instruction.table_name is not None
     assert instruction.created is not None
     assert instruction.modified is not None
 
-    assert user.id is not None
-    assert user.created is not None
-    assert user.modified is not None
-
-
-def test_recipe_create():
-    """ It should create an recipe """
-    title = 'Another one'
-    description = 'Description for another one'
-    table = recipe.db.table('recipes')
-    len_before = len(table.all())
-
-    Recipe.create(title=title, description=description)
-    len_after = len(table.all())
-    assert len(recipe.db.table(recipe.table_name).all()) != 0
-    assert len_before != len_after
-
 
 def test_create_instruction():
-    """It should create an instruction """
-    title = 'New Instruction'
-    recipe_id = instruction.recipe
+    """ It should create an instruction """
+    title = 'Another one'
+    description = 'Description for another one'
     table = instruction.db.table('instructions')
     len_before = len(table.all())
-    assert len_before == 0, 'No instructions yet'
 
-    Instruction.create(title=title, recipe_id=recipe_id)
+    Instruction.create(recipe_id=recipe_id, title=title, description=description)
     len_after = len(table.all())
     assert len(table.all()) != 0, 'Expected at least one instruction in the instructions table'  # NOQA
-    assert len_before != len_after
-    assert len(instruction.db.table(instruction.table_name).all()) != 0
     assert len_before != len_after
 
 
 def test_presence_of_table_names():
     """ Models should have table names """
 
-    assert recipe.table_name is not None
+    assert instruction.table_name is not None
     assert user.table_name is not None
     assert instruction.table_name is not None
 
@@ -63,13 +45,14 @@ def test_presence_of_table_names():
 def test_delete_instruction():
     title = 'Instruction for deletion'
     table = instruction.db.table('instructions')
-    Instruction.create(title=title)
     len_before = len(table.all())
-    assert len_before != 0
+    Instruction.create(recipe_id=recipe_id, title=title)
+    len_after_insert = len(table.all())
+    assert len_before != len_after_insert
 
     rec_id = table.all()[-1].get('id')  # Get last-saved instruction
     Instruction.delete(rec_id)
-    len_after = len(table.all())
+    len_after_deletion = len(table.all())
     found = table.get(where('id') == rec_id)
     assert found is None
-    assert len_before != len_after
+    assert len_after_insert != len_after_deletion
