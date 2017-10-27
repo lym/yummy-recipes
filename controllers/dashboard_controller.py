52859b5801bb4186478c7ad81842aef79d6a1192
Salym Senyonga <salymsash@gmail.com>
2017-10-27 07:03:26 +0300

    Protect dashboard from non-logged in users
    When the dashboard URL is hit by a logged-out user, redirect the user
    to the login page.

--- controllers/dashboard_controller.py
+++ controllers/dashboard_controller.py
@@ -1,3 +1,4 @@
+from flask import session
 from flask import render_template
 from flask.views import View
 
@@ -5,6 +6,8 @@ from models import Recipe
 
 class DashboardController(View):
     def dispatch_request(self):
+        if session.get('user') is None:
+            return render_template('login.html', name='login')
         recipe_coln =  Recipe.ds.storage.get('recipes')
         if (recipe_coln is None) or (len(recipe_coln)) == 0:
             recipes = []
